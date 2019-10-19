f690787b8eba4d7492eb3dc701f8cf94137965e9
shyash <shyash222@gmail.com>
2019-10-20 02:26:08 +0530

    update

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const PORT = process.env.PORT || 8800;
 const IP = process.env.IP || "0.0.0.0";
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 const DBURI =
   "mongodb+srv://yash:hsay@cluster0-mxy94.mongodb.net/test?retryWrites=true&w=majority";
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
+
 mongoose.connection.openUri(DBURI, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
 });
+
+const server = app.listen(PORT, IP, () => {
+  console.log("Server started!");
+});
+
+const io = require("socket.io")(server);
 
 let TopicSchema = new mongoose.Schema({
   title: String,
@@ -17,15 +24,15 @@ let TopicSchema = new mongoose.Schema({
 });
 let Topic = mongoose.model("Topic", TopicSchema);
 
-app.get('/get/topics',(req,res)=>{
-  Topic.find({}, function(err, founded) {
-  res.json(founded);
+io.sockets.on("connection", socket => {
+  console.log("Client : ", socket.id);
+  socket.on("text", data => socket.broadcast.emit("text", data));
+  socket.on("draw", data => socket.broadcast.emit("draw", data));
+  socket.on("disconnect", () => console.log("client has disconnected"));
 });
-})
 
-app.get("*", function(req, res) {
-  res.send("404 URL NOT FOUND");
-});
-app.listen(PORT, IP, () => {
-  console.log("Server started!");
+app.get("/get/topics", (req, res) => {
+  Topic.find({}, function(err, founded) {
+    res.json(founded);
+  });
 });
